3128f38d79d99a1d43eb21de2d230dc212edfc36
Tim <jasper7466@yandex.ru>
2023-11-30 22:19:31 +0500

    feat: solve problem

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -259,8 +259,22 @@ function getCube(num) {
  *   3  => 2
  *   10 => 55
  */
-function getFibonacciNumber(/* index */) {
-  throw new Error('Not implemented');
+function getFibonacciNumber(index) {
+  if (index === 0) {
+    return 0;
+  }
+
+  let fib1 = 0;
+  let fib2 = 1;
+  let fib3 = fib1 + fib2;
+
+  for (let i = 2; i <= index; i += 1) {
+    fib3 = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = fib3;
+  }
+
+  return fib3;
 }
 
 /**
